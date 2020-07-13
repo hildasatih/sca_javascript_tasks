5a31ed4f9aae57677f36fc4a0279e7cd3c645418
hilda <hildasattih@gmail.com>
2020-07-13 22:59:33 +0100

    week 6 added

---
+++ week 5/cat.js
@@ -0,0 +1,29 @@
+
+
+const cat = {
+ stamina: 'tiredness',
+ appetite: 'hunger',
+ satus: 'lonliness',
+ mode: 'happiness',
+ sleep: function(){
+
+ },
+ feed: function(){
+    console.log('hello word') 
+ },
+
+ dispo(){
+     console.log(`the cat is tired`)
+ },
+
+ state(){
+
+ },
+
+ attitude(){
+     console.log()
+ }
+ 
+}
+
+console.log(cat.feed);
